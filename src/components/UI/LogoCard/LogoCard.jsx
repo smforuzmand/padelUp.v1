6e6020b01d4a6e477d31fe8749211a14cf0bad20
smforuzmand <career.forouzmand@gmail.com>
2023-02-11 21:56:41 +0100

    refactor Logo Cards

--- src/components/UI/LogoCard/LogoCard.jsx
+++ src/components/UI/LogoCard/LogoCard.jsx
@@ -4,12 +4,18 @@ import Icon1 from "../../../assets/icons/logo-1.svg";
 import Icon2 from "../../../assets/icons/logo-2.svg";
 import Icon3 from "../../../assets/icons/logo-3.svg";
 
+const logos = [
+  { src: Icon1, alt: "Logo 1" },
+  { src: Icon2, alt: "Logo 2" },
+  { src: Icon3, alt: "Logo 3" },
+];
+
 const LogoCard = () => {
   return (
     <div className={styles.card}>
-      <img src={Icon2} alt="Logo" />
-      <img src={Icon3} alt="Logo" />
-      <img src={Icon1} alt="Logo" />
+      {logos.map((logo, index) => (
+        <img key={index} src={logo.src} alt={logo.alt} />
+      ))}
     </div>
   );
 };
